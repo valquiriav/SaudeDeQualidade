da50cb28ac7660741123f91df44ba47b3304d119
valquiriav <valvitoli@gmail.com>
2021-08-14 16:14:52 -0300

    Paciente primeiro commit

--- src/main/java/br/com/letscode/saudedequalidade/Paciente/Paciente.java
+++ src/main/java/br/com/letscode/saudedequalidade/Paciente/Paciente.java
@@ -1,4 +1,44 @@
 package br.com.letscode.saudedequalidade.Paciente;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "paciente")
+@Table(name = "paciente")
+
 public class Paciente {
+
+    @Id
+    @GeneratedValue
+    @Column(name="identificador_paciente",nullable = false)
+    private Long id;
+
+    @Column(name="nome_paciente",nullable = false)
+    private String nome;
+
+    @Column(name="sobrenome_paciente",nullable = false)
+    private String sobrenome;
+
+    @Column(name="regiao",nullable = false)
+    private String regiao;
+
+    @Column(name="data_nascimento",nullable = false)
+    private LocalDate data_nascimento;
+
+    //TODO colcocar como atributo derivado
+    @Column(name="idade", nullable = false)
+    private int idade;
 }
