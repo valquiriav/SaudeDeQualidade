0b7570a351550c12bd6ba94e0e3b3348558f8a93
Brunoacpe <74093392+brunoacpe@users.noreply.github.com>
2021-08-19 13:18:31 -0300

    Documentando melhor o controller ProfissionalContatoController.

--- src/main/java/br/com/letscode/saudedequalidade/Profissional/ProfissionalContatoController.java
+++ src/main/java/br/com/letscode/saudedequalidade/Profissional/ProfissionalContatoController.java
@@ -1,6 +1,11 @@
 package br.com.letscode.saudedequalidade.Profissional;
 
+import br.com.letscode.saudedequalidade.Exceptions.ExceptionResponse;
 import br.com.letscode.saudedequalidade.Exceptions.ProfissionalNaoExistenteException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@Api(value="Operações relacionadas com os contatos dos profissionais.")
 @RestController
 @RequestMapping("/contato")
 public class ProfissionalContatoController {
@@ -25,17 +30,31 @@ public class ProfissionalContatoController {
         repository = profissionalRepository;
     }
 
+    @ApiOperation(value="Procurar os contatos pelo id do profissional.")
+    @ApiResponses(value = {
+            @ApiResponse(code=200, message="Contatos encontrados com sucesso.", response = Profissional.class),
+            @ApiResponse(code=404, message = "Não existe nenhum profissional com este ID.", response = ExceptionResponse.class)
+    })
     @GetMapping("/findContatosByIdProfissional/{id}")
     public ResponseEntity<List<ProfissionalContatoDTO>> findByIdProfissional(@PathVariable Long id){
         return ResponseEntity.ok().body(service.findProfissionalContatoByidProfissional(repository.findById(id).orElseThrow(ProfissionalNaoExistenteException::new)));
     }
 
+    @ApiOperation(value="Criar um novo contato.")
+    @ApiResponses(value = {
+            @ApiResponse(code=201, message="Contatos encontrados com sucesso.", response = Profissional.class),
+            @ApiResponse(code=404, message = "Não existe nenhum profissional com este ID.", response = ExceptionResponse.class)
+    })
     @PostMapping("/criarContato/{idProfissional}")
     public ResponseEntity<ProfissionalContato> createProfissionalContato(@PathVariable Profissional idProfissional ,@RequestBody Integer telefone){
         ProfissionalContato pc = new ProfissionalContato(telefone,idProfissional);
         return ResponseEntity.status(201).body(service.createContatoProfissional(pc));
     }
 
+    @ApiOperation(value="Buscar todos os contatos.")
+    @ApiResponses(value = {
+            @ApiResponse(code=200, message="Contatos encontrados com sucesso.", response = Profissional.class)
+    })
     @GetMapping("/listarContatos")
     public ResponseEntity<List<ProfissionalContato>> listarContatos(){
         return ResponseEntity.ok().body(
